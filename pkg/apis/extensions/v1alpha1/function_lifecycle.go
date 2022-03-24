1c4ca75bdd4a786110781308849109c4a38a43bc
Timon Wong <timon86.wang@gmail.com>
2022-03-24 12:33:46 +0800

    Add reconciler boilerplate (#3)
    Signed-off-by: Timon Wong <timon86.wang@gmail.com>

--- pkg/apis/extensions/v1alpha1/function_lifecycle.go
+++ pkg/apis/extensions/v1alpha1/function_lifecycle.go
@@ -1,23 +1,36 @@
-/*
-Copyright 2022 The hyperfunction Authors
+// Copyright 2022 The hyperfunction Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package v1alpha1
 
-import "k8s.io/apimachinery/pkg/runtime/schema"
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"knative.dev/pkg/apis"
+)
+
+var functionCondSet = apis.NewLivingConditionSet()
 
 // GetGroupVersionKind implements kmeta.OwnerRefable
 func (*Function) GetGroupVersionKind() schema.GroupVersionKind {
 	return SchemeGroupVersion.WithKind("Function")
+}
+
+// GetConditionSet implements the duckv1.KRShaped interface.
+func (in *Function) GetConditionSet() apis.ConditionSet {
+	return functionCondSet
+}
+
+func (in *FunctionStatus) InitializeConditions() {
+	functionCondSet.Manage(in).InitializeConditions()
 }
---
+++ pkg/reconciler/function/controller.go
@@ -0,0 +1,44 @@
+// Copyright 2022 The hyperfunction Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package function
+
+import (
+	"context"
+
+	kubeclient "knative.dev/pkg/client/injection/kube/client"
+	"knative.dev/pkg/configmap"
+	"knative.dev/pkg/controller"
+	"knative.dev/pkg/logging"
+
+	functioninformer "github.com/hyperfunction/hyperfunction/pkg/client/injection/informers/extensions/v1alpha1/function"
+	functionreconciler "github.com/hyperfunction/hyperfunction/pkg/client/injection/reconciler/extensions/v1alpha1/function"
+)
+
+// NewController creates a Reconciler and returns the result of NewImpl.
+func NewController(ctx context.Context, cmw configmap.Watcher) *controller.Impl {
+	logger := logging.FromContext(ctx)
+
+	functionInformer := functioninformer.Get(ctx)
+
+	r := &Reconciler{
+		kubeclient: kubeclient.Get(ctx),
+	}
+	impl := functionreconciler.NewImpl(ctx, r)
+
+	logger.Info("Setting up event handlers.")
+
+	functionInformer.Informer().AddEventHandler(controller.HandleAll(impl.Enqueue))
+	return impl
+}
